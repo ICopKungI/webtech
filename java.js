03cd856ae82fd0a789d86979ab2a652c99c2e8b6
Kotchatan Chatchoeidaeng <44992872+ICopKungI@users.noreply.github.com>
2019-10-12 17:26:58 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -4,10 +4,12 @@ function menu(x) {
         let drop = document.querySelector("#dropdownicon1");
         if (menu.style.visibility == "visible") {
             menu.style.visibility = "hidden";
-            drop.setAttribute("class", "")
+            drop.setAttribute("class", "");
         } else {
             menu.style.visibility = "visible"
-            drop.setAttribute("class", "rotateimg180")
+            drop.setAttribute("class", "rotateimg180");
+            document.getElementById("menu22").style.visibility = "hidden";
+            document.querySelector("#dropdownicon2").setAttribute("class", "");
         }
     }
     if (x == '2') {
@@ -15,21 +17,29 @@ function menu(x) {
         let drop = document.querySelector("#dropdownicon2");
         if (menu.style.visibility == "visible") {
             menu.style.visibility = "hidden";
-            drop.setAttribute("class", "")
+            drop.setAttribute("class", "");
         } else {
-            menu.style.visibility = "visible"
-            drop.setAttribute("class", "rotateimg180")
+            menu.style.visibility = "visible";
+            drop.setAttribute("class", "rotateimg180");
+            document.getElementById("menu21").style.visibility = "hidden";
+            document.querySelector("#dropdownicon1").setAttribute("class", "");
         }
     }
-    if (x == '3') {
-        let menu = document.getElementById("menu23");
-        let drop = document.querySelector("#dropdownicon3");
-        if (menu.style.visibility == "visible") {
-            menu.style.visibility = "hidden";
-            drop.setAttribute("class", "")
-        } else {
-            menu.style.visibility = "visible"
-            drop.setAttribute("class", "rotateimg180")
-        }
-    }
+    // if (x == '3') {
+    //     let menu = document.getElementById("menu23");
+    //     let drop = document.querySelector("#dropdownicon3");
+    //     if (menu.style.visibility == "visible") {
+    //         menu.style.visibility = "hidden";
+    //         drop.setAttribute("class", "")
+    //     } else {
+    //         menu.style.visibility = "visible"
+    //         drop.setAttribute("class", "rotateimg180")
+    //     }
+    // }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
