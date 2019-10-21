e2f755cac7ead5b1e2a03c534785a5e72f279de1
PanupongSoongwiriya <42969697+PanupongSoongwiriya@users.noreply.github.com>
2019-10-21 10:06:55 +0700

    chang position photo

--- js.js
+++ js.js
@@ -13,43 +13,39 @@ function start(type, num) {
 }
 
 function write(myObj, type, num) {
-    let ans = "";
+    let text = "";
     let main = document.getElementById("main");
     main.innerHTML = "";
     if (type == 1) {
-        ans += ("<p class=\"head1\">" + myObj.contagion[num].disease[0] + "</p><br>\n");
-        ans += loop_write(myObj.contagion[num].cause);
-        ans += loop_write(myObj.contagion[num].symptom);
-        ans += loop_write(myObj.contagion[num].therapy);
-        ans += loop_write(myObj.contagion[num].protect);
-        ans += loop_write_img(myObj.contagion[num].img);
+        text += ("<p class=\"head1\">" + myObj.contagion[num].disease[0] + "</p><br>\n");
+        text += loop_write(myObj.contagion[num].cause);
+        text += ('<img class="photo" src=\"' + myObj.contagion[num].img[0] + "\">\n");
+        text += loop_write(myObj.contagion[num].symptom);
+        text += ('<img class="photo" src=\"' + myObj.contagion[num].img[1] + "\">\n");
+        text += loop_write(myObj.contagion[num].therapy);
+        text += ('<img class="photo" src=\"' + myObj.contagion[num].img[2] + "\">\n");
+        text += loop_write(myObj.contagion[num].protect);
     } else {
-        ans += ('<p class=\"head1\">' + myObj.n_contagion[num].disease[0] + '</p><br>\n');
-        ans += ('<p class=\"head2\">&emsp;&emsp;&emsp;' + myObj.n_contagion[num].disease[1] + '</p><br>\n');
-        ans += loop_write(myObj.n_contagion[num].cause);
-        ans += loop_write(myObj.n_contagion[num].symptom);
-        ans += loop_write(myObj.n_contagion[num].therapy);
-        ans += loop_write(myObj.n_contagion[num].protect);
-        ans += loop_write_img(myObj.n_contagion[num].img);
+        text += ('<p class=\"head1\">' + myObj.n_contagion[num].disease[0] + '</p><br>\n');
+        text += ('<p class=\"head2\">&emsp;&emsp;&emsp;' + myObj.n_contagion[num].disease[1] + '</p><br>\n');
+        text += loop_write(myObj.n_contagion[num].cause);
+        text += ('<img class="photo" src=\"' + myObj.n_contagion[num].img[0] + "\">\n");
+        text += loop_write(myObj.n_contagion[num].symptom);
+        text += ('<img class="photo" src=\"' + myObj.n_contagion[num].img[1] + "\">\n");
+        text += loop_write(myObj.n_contagion[num].therapy);
+        text += ('<img class="photo" src=\"' + myObj.n_contagion[num].img[2] + "\">\n");
+        text += loop_write(myObj.n_contagion[num].protect);
     }
-    main.innerHTML = ans;
+    main.innerHTML = text;
 }
 
 function loop_write(myObj) {
-    let ans = "";
-    ans += ('<p class=\"head2\"><b>&emsp;&emsp;&emsp;' + myObj[0] + '</b> ' + myObj[1] + '</p>\n');
-    ans += ('<ul>\n');
+    let text = "";
+    text += ('<br><p class=\"head2\"><b>&emsp;&emsp;&emsp;' + myObj[0] + '</b> ' + myObj[1] + '</p>\n');
+    text += ('<ul>\n');
     for (let i = 2; i < myObj.length; i++) {
-        ans += ('<li>' + myObj[i] + '\n');
+        text += ('<li>' + myObj[i] + '\n');
     }
-    ans += ('</ul>\n');
-    return ans
-}
-
-function loop_write_img(myObj) {
-    let ans = "";
-    for (let i = 0; i < myObj.length; i++) {
-        ans += ('<img src=\"' + myObj[i] + "\" width=\"300px\" height=\"300px\">\n");
-    }
-    return ans;
+    text += ('</ul><br>\n');
+    return text
 }
