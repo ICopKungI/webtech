ecf7aee55c82bf2975a013c4b6ef6ad4e66b381c
PanupongSoongwiriya <42969697+PanupongSoongwiriya@users.noreply.github.com>
2019-10-29 21:38:58 +0700

    เกือบเสร็จ

--- java.js
+++ java.js
@@ -4,6 +4,16 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function setdiv_index() {
+    document.getElementById("index").style.height = "100%";
+    var x = document.getElementById('nav');
+    var style = window.getComputedStyle(x, null).getPropertyValue('height');
+    var height = parseFloat(style);
+    var div = document.querySelector('#index');
+    div.style.top = (height + 35) + 'px';
+    div.style.marginBottom = (height + 75) + 'px';
+}
+
 function setdiv() {
     color();
     var x = document.getElementById('nav');
@@ -18,15 +28,18 @@ function color() {
     x = Math.floor(Math.random() * 5) + 1;
     let div_color = document.getElementById("color");
     div_color.innerHTML = "";
-    div_color.innerHTML = "<link rel=\"stylesheet\" href=\"color" + x + ".css\">";
+    div_color.innerHTML = "<link rel=\"stylesheet\" href=\"css/color" + x + ".css\">";
 }
 
 //ส่วน json
 
 function start(type, num) {
-    try {document.getElementById("index").id = "main";}catch (err) {}
-    document.getElementById("main").style.height = "100%";
-    let requestURL = 'data.json';
+    try {
+        document.getElementById("main").style.height = "100%";
+    } catch (err) {
+        document.getElementById("index").id = "main";
+        document.getElementById("main").style.height = "100%";
+    }
     let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState == 4 && request.status == 200) {
@@ -34,7 +47,7 @@ function start(type, num) {
             write(myJSON, type, num);
         }
     };
-    request.open("GET", requestURL, true);
+    request.open("GET", 'data.json', true);
     request.send();
 }
 
@@ -42,19 +55,25 @@ function write(myObj, type, num) {
     let text = "";
     let main = document.getElementById("main");
     main.innerHTML = "";
-    if (type == 1) {
+    if (type == 1) {//โรคติดต่อ
         text += img(myObj.contagion[num].disease, myObj.contagion[num].img);
-        text += loop_write(myObj.contagion[num].cause,);
-        text += loop_write(myObj.contagion[num].symptom,);
-        text += loop_write(myObj.contagion[num].therapy,);
+        text += loop_write(myObj.contagion[num].cause);
+        text += loop_write(myObj.contagion[num].symptom);
+        text += loop_write(myObj.contagion[num].therapy);
         text += loop_write(myObj.contagion[num].protect);
-    } else {
+    } else if (type == 0) {//โรคไม่ติดต่อ
         text += img(myObj.n_contagion[num].disease, myObj.n_contagion[num].img);
         text += ('<p class=\"head2\">&emsp;&emsp;&emsp;' + myObj.n_contagion[num].disease[1] + '</p><br>\n');
         text += loop_write(myObj.n_contagion[num].cause);
         text += loop_write(myObj.n_contagion[num].symptom);
         text += loop_write(myObj.n_contagion[num].therapy);
         text += loop_write(myObj.n_contagion[num].protect);
+    } else {//คณะผู้จัดทำหรืออ้างอิง
+        if (num == "producer") {
+            text += other(myObj.other.producer, 0);
+        } else {
+            text += other(myObj.other.references, 1);
+        }
     }
     main.innerHTML = text;
     setdiv();
@@ -70,9 +89,9 @@ function img(myObj1, myObj2) {
     for (let i = 0; i < myObj2.length; i++) {
         text += "<li data-target=\"#myCarousel\" data-slide-to=\"" + i + "\"";
         if (i == 0) {
-            text +=  "class=\"active\" ";
+            text += "class=\"active\" ";
         }
-        text +=  "></li>\n";
+        text += "></li>\n";
     }
     text += "</ol>\n";
 
@@ -80,20 +99,20 @@ function img(myObj1, myObj2) {
     for (let i = 0; i < myObj2.length; i++) {
         text += "<div class=\"item";
         if (i == 0) {
-            text +=  " active";
+            text += " active";
         }
-        text +=  "\">\n";
+        text += "\">\n";
         text += "<img src=\"" + myObj2[i] + "\" class=\"photo\">\n</div>";
     }
     text += "</div>\n";
     text += "<a class=\"left carousel-control\" href=\"#myCarousel\" data-slide=\"prev\">\n<span class=\"glyphicon glyphicon-chevron-left\"></span>\n<span class=\"sr-only\">Previous</span>\n</a>\n<a class=\"right carousel-control\" href=\"#myCarousel\" data-slide=\"next\">\n<span class=\"glyphicon glyphicon-chevron-right\"></span>\n<span class=\"sr-only\">Next</span>\n</a>\n";
 
     text += "</div>\n";
-    text += "</div>\n";
+    text += "</div><br>\n";
     return text
 }
 
-function loop_write(myObj, num) {
+function loop_write(myObj) {
     let text = "";
     text += ('<br><p class=\"head2\"><b>&emsp;&emsp;&emsp;' + myObj[0] + '</b> ' + myObj[1] + '</p>\n');
     text += ('<ul>\n');
@@ -102,4 +121,26 @@ function loop_write(myObj, num) {
     }
     text += ('</ul><br>\n');
     return text
+}
+
+function other(myObj, check) {
+    let text = "";
+    let main = document.getElementById("main");
+    main.innerHTML = "";
+    text += "<div class=\"container\">\n<p class=\"head1\">" + myObj.topic + "</p><br>\n";
+
+    for (let i = 0; i < myObj.content.length; i++) {
+        text += "<br><div class=\"row text-center\">\n";
+        text += "<div class=\"col-xl-6 col-lg-6 col-md-12 col-sm-12\"><center><img src=\"" + myObj.img[i] + "\" class=\"photo_other\"></center></div>\n";
+        text += "<div class=\"col-xl-6 col-lg-6 col-md-12 col-sm-12\">";
+        if (check) {
+            text += "<a  href=\"https://" + myObj.content[i] + "\"  target=\"_blank\" class=\"other\">" +  myObj.content[i] + "</a></div>\n";
+        }else{
+            text += "<p class=\"other\">" +  myObj.content[i] + "</p></div>\n";
+        }
+        text += '</div><br>\n';
+    }
+    text += '</div>\n';
+    return text;
+    
 }
